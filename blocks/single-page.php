36eb9244bfbeaa9997fd05761435e1ea94116c11
Vili Ståhlberg <vkstah@utu.fi>
2022-07-08 13:43:43 +0300

    Separate content based on post type

--- blocks/single-page.php
+++ blocks/single-page.php
@@ -6,9 +6,9 @@ function getContent() {
   } elseif ( is_checkout() ) {
     get_template_part( 'template-parts/checkout' );
   } else {
-    get_template_part( 'template-parts/content' );
+    get_template_part( 'template-parts/content', get_post_type() );
   }
-  
+
 }
 ?>
 
