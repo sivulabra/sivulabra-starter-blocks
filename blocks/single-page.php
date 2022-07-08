d12410f07e9b908dd76f1572fbfa2006e8a35346
Vili Ståhlberg <vkstah@utu.fi>
2022-07-08 13:11:38 +0300

    Add WooCommerce page logic and WooCommerce styling

--- blocks/single-page.php
+++ blocks/single-page.php
@@ -1,19 +1,48 @@
+<?php
+function getContent() {
+  if ( is_cart() ) {
+    ?>
+
+    <div class="container">
+      <?php if ( have_posts() ) {
+        while ( have_posts() ) {
+          the_post();
+          the_content();
+        }
+      } ?>
+    </div>
+
+    <?php
+  } elseif ( is_checkout() ) {
+    ?>
+    
+    <div class="container">
+      <?php if ( have_posts() ) {
+        while ( have_posts() ) {
+          the_post();
+          the_content();
+        }
+      } ?>
+    </div>
+
+    <?php
+  } else {
+    ?>
+
+    <div class="container">
+      <?php if ( have_posts() ) {
+        while ( have_posts() ) {
+          the_post();
+          the_content();
+        }
+      } ?>
+    </div>
+
+    <?php
+  }
+}
+?>
+
 <main id="site-main" class="site-main">
-  <?php
-    if ( is_cart() ) {
-      if ( have_posts() ) {
-        while ( have_posts() ) {
-          the_post(); 
-          the_content();
-        }
-      }
-    } else {
-      if ( have_posts() ) {
-        while ( have_posts() ) {
-          the_post(); 
-          the_content();
-        }
-      }
-    }
-  ?>
+  <?php getContent(); ?>
 </main>
