e39c6be9edd16ab95f7c82a096208193bf446c7e
Vili Ståhlberg <vkstah@utu.fi>
2022-07-18 10:04:10 +0300

    Add asset versioning for cache busting

--- inc/assets.php
+++ inc/assets.php
@@ -6,10 +6,10 @@ function loadAssets() {
   wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap', false );
 
   // Enqueue main CSS
-  wp_enqueue_style( 'sivulabra-main-styles', get_theme_file_uri( '/build/index.css' ) );
+  wp_enqueue_style( 'sivulabra-main-styles', get_theme_file_uri( '/build/index.css' ), NULL, filemtime( get_theme_file_path( '/build/index.css' ) ) );
 
   // Enqueue main JS
-  wp_enqueue_script( 'sivulabra-main-scripts', get_theme_file_uri( '/build/index.js' ), NULL, NULL, true );
+  wp_enqueue_script( 'sivulabra-main-scripts', get_theme_file_uri( '/build/index.js' ), ['jquery'], filemtime( get_theme_file_path( '/build/index.js' ) ), true );
   
 }
 add_action( 'wp_enqueue_scripts', 'loadAssets' );
