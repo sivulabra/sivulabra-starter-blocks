70bf21fb38e928f87995e7838e4c05e90e0c2b67
Vili Ståhlberg <vkstah@utu.fi>
2022-07-18 10:03:26 +0300

    Add the defer tag to scripts for additional performance

--- inc/assets.php
+++ inc/assets.php
@@ -1,4 +1,5 @@
 <?php
+// Main asset loading
 function loadAssets() {
   
   // Enqueue fonts
@@ -12,3 +13,14 @@ function loadAssets() {
   
 }
 add_action( 'wp_enqueue_scripts', 'loadAssets' );
+
+// Add the defer tag to script enqueues
+add_filter( 'script_loader_tag', function( $url ) {
+
+  if (is_admin()) return $url;
+  if (false === strpos( $url, '.js' )) return $url;
+  if (strpos($url, 'jquery.js')) return $url;
+  if (strpos($url, 'jquery.min.js')) return $url;
+  return str_replace(' src', ' defer src', $url);
+
+}, 10 );
